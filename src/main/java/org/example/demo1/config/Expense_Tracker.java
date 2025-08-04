7522946cf8445d49168daf80de511b92f1da9eef
Murtaza Ahmed <145542613+Murtaza-L200954@users.noreply.github.com>
2025-08-04 14:45:07 +0500

    Delete src/main directory

--- src/main/java/org/example/demo1/config/Expense_Tracker.java
+++
@@ -1,8 +0,0 @@
-package org.example.demo1.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class Expense_Tracker extends Application {
-}
